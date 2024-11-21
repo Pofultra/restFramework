a7b6969cd8c526f7fcf4c85a07e20f232a8ae96b
Pavel <hivikipof@gmail.com>
2024-11-21 05:18:43 -0500

    feat: add exceptions handling and db-logger

--- snippets/views.py
+++ snippets/views.py
@@ -9,15 +9,26 @@ from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework import viewsets
 from rest_framework.decorators import action
+import logging
+
+db_logger = logging.getLogger("db")
+
+db_logger.info("info message")
+db_logger.warning("warning message")
+
+
 
 @api_view(["GET"])
 def api_root(request, format=None):
-    return Response(
-        {
-            "users": reverse("user-list", request=request, format=format),
-            "snippets": reverse("snippet-list", request=request, format=format),
-        }
-    )
+    try:
+        return Response(
+            {
+                "users": reverse("user-list", request=request, format=format),
+                "snippets": reverse("snippet-list", request=request, format=format),
+            }
+        )
+    except Exception as e:
+        db_logger.exception(str(e))
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -29,8 +40,6 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserSerializer
 
 
-
-
 class SnippetViewSet(viewsets.ModelViewSet):
     """
     This ViewSet automatically provides `list`, `create`, `retrieve`,
@@ -38,15 +47,21 @@ class SnippetViewSet(viewsets.ModelViewSet):
 
     Additionally we also provide an extra `highlight` action.
     """
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
     def highlight(self, request, *args, **kwargs):
         snippet = self.get_object()
-        return Response(snippet.highlighted)
+        try:
+            return Response(snippet.highlighted)
+        except Exception as e:
+            db_logger.exception(str(e))
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+        try:
+            serializer.save(owner=self.request.user)
+        except Exception as e:
+            db_logger.exception(str(e))
