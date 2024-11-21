2e3ca207bdb3f4a53b5b85fe87de03ee869cf976
Pavel <hivikipof@gmail.com>
2024-11-21 04:41:15 -0500

    feat: migrate to postgres

--- restFramework/settings.py
+++ restFramework/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'restFramework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tutorial_drf',
+        'USER': 'postgres',
+        'PASSWORD': 'Postgres123*',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
